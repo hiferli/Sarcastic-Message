ae9cd339e92df70247b8522abc88cf770f800e99
Hiferli <joshi.ishaan.2001@gmail.com>
2022-03-17 15:00:15 +0530

    Added a Copy Message button and removed Console.log(s)

--- javascript.js
+++ javascript.js
@@ -1,4 +1,29 @@
+let afterCopy = "Copied ✅";
+let beforeCopy = "Copy Message 📝";
+
 function sarcast1() {
+  document.getElementById("copy").innerText = beforeCopy;
+  let s = document.getElementById("string").value.toLowerCase();
+  // console.log(s);
+  
+  let another_s = "";
+  
+  for (let i = 0; i < s.length; i++) {
+    let char = s.charAt(i);
+    
+    if (i % 2 == 0) {
+      another_s = another_s.concat(char.toUpperCase());
+    } else {
+      another_s = another_s.concat(char);
+    }
+  }
+  
+  // console.log(another_s);
+  document.getElementById("output").innerText = another_s;
+}
+
+function sarcast2() {
+  document.getElementById("copy").innerText = beforeCopy;
   let s = document.getElementById("string").value.toLowerCase();
   // console.log(s);
 
@@ -8,9 +33,9 @@ function sarcast1() {
     let char = s.charAt(i);
 
     if (i % 2 == 0) {
-      another_s = another_s.concat(char.toUpperCase());
-    } else {
       another_s = another_s.concat(char);
+    } else {
+      another_s = another_s.concat(char.toUpperCase());
     }
   }
 
@@ -18,22 +43,11 @@ function sarcast1() {
   document.getElementById("output").innerText = another_s;
 }
 
-function sarcast2() {
-  let s = document.getElementById("string").value.toLowerCase();
-  console.log(s);
+function copyMessage(){
+  let finalMessage = document.getElementById("output").innerText;
+  navigator.clipboard.writeText(finalMessage);
+  console.log(finalMessage);
 
-  let another_s = "";
+  document.getElementById("copy").innerText = afterCopy;
 
-  for (let i = 0; i < s.length; i++) {
-    let char = s.charAt(i);
-
-    if (i % 2 == 0) {
-      another_s = another_s.concat(char);
-    } else {
-      another_s = another_s.concat(char.toUpperCase());
-    }
-  }
-
-  console.log(another_s);
-  document.getElementById("output").innerText = another_s;
 }
